19de4659e4acd76370fb536c14e97e7b0658bd59
joy_li <woaiyinle520@163.com>
2021-09-27 17:50:42 +0800

    constom hook debounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,33 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { SearchPanel } from './search-panel';
 import { List } from './list';
 import * as qs from 'qs'
-import { cleanObject } from 'utils';
+import { cleanObject, useMount, useDebounce } from 'utils';
 
 const apiUrl = process.env.REACT_APP_API_URL
 
 export const ProjectListScreen = () => {
+    const [users, setUsers] = useState([])
     const [param, setParam] = useState({
         name: '',
         personId: ''
     })
-    const [users, setUsers] = useState([])
+
+    const deboucedParam = useDebounce(param, 400)
     const [list, setList] = useState([])
 
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(deboucedParam))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
+    }, [deboucedParam])
 
-    useEffect(() => {
+
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
+    })
 
     console.log('hello');
     return (
